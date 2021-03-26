090d968bc06cd2be672291024011628d3880b1c0
yuma-nemoto <yuma-nemoto@dac.co.jp>
2021-03-26 09:05:34 +0900

    app: replace string with path

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use anyhow::{bail, ensure, Context, Result};
 use clap::Clap;
 use std::fs::File;
 use std::io::{stdin, BufRead, BufReader};
+use std::path::PathBuf;
 
 
 struct RpnCalculator(bool);
@@ -62,7 +63,7 @@ struct Opts {
     verbose: bool,
 
     #[clap(name = "FILE")]
-    formula_file: Option<String>,
+    formula_file: Option<PathBuf>,
 }
 
 fn main() {
