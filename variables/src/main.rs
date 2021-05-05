9f1b7e89092c0abb57ba084dbfe8a84667d90c55
yuma-nemoto <yuma-nemoto@dac.co.jp>
2021-05-05 23:55:56 +0900

    val: return val

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,8 +1,12 @@
 fn main() {
-    another_function(5, 6);
+    another_function(five(), 6);
 }
 
 fn another_function(x: i32, y: i32) {
     println!("The value of x is: {}", x);
     println!("The value of y is: {}", y);
+}
+
+fn five() -> i32 {
+    5
 }
