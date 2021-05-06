6d28d9a154bc125c4a2e42c5ef411feb31f41b86
yuma-nemoto <yuma-nemoto@dac.co.jp>
2021-05-07 08:27:16 +0900

    ln: change function to generic but compile failed

--- largest-number/src/main.rs
+++ largest-number/src/main.rs
@@ -1,16 +1,4 @@
-fn largest(list: &[i32]) -> i32 {
-    let mut largest = list[0];
-
-    for &item in list.iter() {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
-}
-
-fn largest_char(list: &[char]) -> char {
+fn largest<T>(list: &[T]) -> T {
     let mut largest = list[0];
 
     for &item in list.iter() {
