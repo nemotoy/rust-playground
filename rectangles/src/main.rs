f5fc301020c963356453897d0cf4eedacb084aef
yuma-nemoto <yuma-nemoto@dac.co.jp>
2021-05-06 15:44:18 +0900

    rec: associated functions

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -8,6 +8,18 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    // 関連関数（selfを引数に取らない）
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -19,4 +31,15 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         rect.area()
     );
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect.can_hold(&rect2));
+
+    let square = Rectangle::square(50);
+
+    println!("square {:?}", square);
 }
