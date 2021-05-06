7a4e74fbdbfd37f96314338a35c94f2e22adf748
yuma-nemoto <yuma-nemoto@dac.co.jp>
2021-05-07 08:21:55 +0900

    ln: search chars

--- largest-number/src/main.rs
+++ largest-number/src/main.rs
@@ -10,6 +10,18 @@ fn largest(list: &[i32]) -> i32 {
     largest
 }
 
+fn largest_char(list: &[char]) -> char {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
     let result = largest(&number_list);
@@ -18,4 +30,8 @@ fn main() {
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
     let result = largest(&number_list);
     println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest_char(&char_list);
+    println!("The largest char is {}", result);
 }
