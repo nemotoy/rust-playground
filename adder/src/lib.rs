e1cfc7e449cdf8fccb5d1f7677ef87226638aaaa
yuma-nemoto <yuma-nemoto@dac.co.jp>
2021-05-07 20:00:57 +0900

    adr: add custom message to assertion

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -2,6 +2,10 @@ pub fn add_two(a: i32) -> i32 {
     a + 2
 }
 
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -9,5 +13,14 @@ mod tests {
     #[test]
     fn it_adds_two() {
         assert_eq!(4, add_two(2));
+    }
+    #[test]
+    fn greeting_contains_name(){
+        let result = greeting("Tom");
+        assert!(
+            result.contains("Tom"),
+            "Greeting did not contain name, value was {}",
+            result
+        );
     }
 }
