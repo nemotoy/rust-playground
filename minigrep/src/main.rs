37a770a1f458ca5a28d3fc4c93aff6beb72e929f
yuma-nemoto <yuma-nemoto@dac.co.jp>
2021-05-07 22:19:17 +0900

    mg: add function that parses args

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,17 +3,29 @@ use std::{env, fs::File, io::Read};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let mut f = File::open(filename).expect("file not found");
+    let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)
         .expect("something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    // ライフタイム管理とメモリ・時間消費のトレードオフ
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config {query, filename}
 }
