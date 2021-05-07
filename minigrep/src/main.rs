a09316a4ac0007e69d0e5d1893ded588d3f915bc
yuma-nemoto <yuma-nemoto@dac.co.jp>
2021-05-08 07:50:13 +0900

    mg: use result

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,9 +1,12 @@
-use std::{env, fs::File, io::Read};
+use std::{env, fs::File, io::Read, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err|{
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -23,14 +26,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
         // ライフタイム管理とメモリ・時間消費のトレードオフ
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        return Ok(Config { query, filename });
     }
 }
