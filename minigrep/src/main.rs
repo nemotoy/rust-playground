3a05122d37a07c528b615e492483346867cf1c33
yuma-nemoto <yuma-nemoto@dac.co.jp>
2021-05-07 22:10:40 +0900

    mg: read file and print text

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,19 @@
+use std::{env, fs::File, io::Read};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+
+    let mut f = File::open(filename).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
